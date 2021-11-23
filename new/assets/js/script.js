2d6818b66ffaf9e81db7f591be1f447b63aa38b5
Manu Chardonnens <manuchardonnens@Manus-MacBook-Pro.local>
2021-11-23 13:31:14 +0100

    better js

--- new/assets/js/script.js
+++ new/assets/js/script.js
@@ -1,8 +1,28 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
+function clickHandler(e) {
   const togglerEl = document.getElementById('toggler');
-  document.querySelectorAll("a").forEach((anchor) => {
-    anchor.addEventListener('click', () => {
-      togglerEl.click();
-    });
+  const submenus = document.querySelectorAll("nav ul li ul");
+
+  togglerEl.checked = false;
+  submenus.forEach((submenu) => {
+    let copy = submenu.cloneNode(true);
+    let parent;
+
+    if (submenu.parentNode) {
+      parent = submenu.parentNode;
+      parent.removeChild(submenu);
+      setTimeout(() => {
+        parent.appendChild(copy);
+        linksCloseNav();
+      }, 1000);
+    }
   });
+}
+function linksCloseNav() {
+  document.querySelectorAll("nav ul li a").forEach((anchor) => {
+    anchor.removeEventListener('click', clickHandler);
+    anchor.addEventListener('click', clickHandler);
+  });
+}
+document.addEventListener("DOMContentLoaded", function(event) {
+  linksCloseNav();
 });
