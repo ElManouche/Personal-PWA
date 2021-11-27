aa0945f7cdeda0c78f756c9b39d4642aeab06a5b
Manu Chardonnens <manuchardonnens@Manus-MacBook-Pro.local>
2021-11-28 00:18:31 +0100

    dynamic nav

--- new/assets/js/script.js
+++ new/assets/js/script.js
@@ -1,30 +1,74 @@
 // JS TO USE THE MENU AS A SINGLE PAGE WITH SCROLL
-function subnavClickHandler(e) {
-  const toggler = document.getElementById('toggler');
-  if(toggler.checked === true) {
+function linkClickedHandler(e) {
+  e.preventDefault();
+  const toggler = document.getElementById('toggler'),
+        mobileOnlyElm = document.querySelector('.wrapper .mobile-navbar'),
+        isMobile = mobileOnlyElm.computedStyleMap().get('display').value === 'block',
+        timeout = ((isMobile && toggler.checked))? 300 : 10;
+  let   target = e.target;
+
+  // Close the mobile menu
+  if(isMobile && toggler.checked === true) {
     setTimeout(() => toggler.click(), 50);
   }
 
-  if (e.target.parentNode.parentNode.parentNode) {
-    const ul = e.target.parentNode.parentNode,
-          copy = ul.cloneNode(true),
-          parent = ul.parentNode;
+  if (target.nodeName !== 'A') {
+    target = target.closest("a");
+  }
 
-    if (parent.nodeName !== 'NAV') {
-      setTimeout(() => parent.removeChild(ul), 100);
-      setTimeout(() => {
-        parent.appendChild(copy);
-        initLinksCloseNav();
-      }, 1000);
-    }
+  // Scroll to the anchor
+  setTimeout(() => {
+    location.hash = "";
+    location.hash = target.getAttribute('href');
+  }, timeout);
+
+  const ul = target.parentNode.parentNode,
+        copy = ul.cloneNode(true),
+        parent = ul.parentNode;
+
+  if (parent.nodeName === 'LI') {
+    setTimeout(() => parent.removeChild(ul), 100);
+    setTimeout(() => {
+      parent.appendChild(copy);
+      initLinksCloseNav();
+    }, 1000);
+  }
+
+}
+
+function toggleHandler() {
+  if(toggler.checked === true) {
+    document.documentElement.classList.add('no-scroll');
+  } else {
+    document.documentElement.classList.remove('no-scroll');
   }
 }
 function initLinksCloseNav() {
-  document.querySelectorAll("nav ul li a").forEach((anchor) => {
-    anchor.removeEventListener('click', subnavClickHandler);
-    anchor.addEventListener('click', subnavClickHandler);
+    Array.from(document.querySelectorAll("a[href*='#']:not([href='#'])")).filter((link) => link.getAttribute('href').startsWith('#')).forEach((link) => {
+    link.removeEventListener('click', linkClickedHandler);
+    link.addEventListener('click', linkClickedHandler);
   });
 }
-document.addEventListener("DOMContentLoaded", function(event) {
+function init() {
   initLinksCloseNav();
+  document.getElementById('toggler').addEventListener('click', toggleHandler);
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  init();
+});
+
+// Animate the desktop navbar
+document.addEventListener("scroll", evt => {
+  const pos = window.scrollY; //evt.target.scrollTop;
+  const container = document.querySelector(".container");
+  if((pos) > 500) {
+    container.classList.remove('up');
+  } else if((pos) > 0) {
+    container.classList.remove('top');
+    container.classList.add('up');
+  } else { // === 0
+    container.classList.add('top');
+    container.classList.remove('up');
+  }
 });
