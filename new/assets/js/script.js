c7c12c5b805cf6b7d03fe88412dbe0072f1a0705
Manu Chardonnens <manuchardonnens@Manus-MacBook-Pro.local>
2021-12-02 10:55:36 +0100

    throttle scroll

--- new/assets/js/script.js
+++ new/assets/js/script.js
@@ -57,6 +57,37 @@ const containerObserverCallback = (entries, observer) => {
   });
 };
 
+const throttle = (func, wait) => {
+  var context, args, result;
+  var timeout = null;
+  var previous = 0;
+  var later = function() {
+    previous = Date.now();
+    timeout = null;
+    result = func.apply(context, args);
+    if (!timeout) context = args = null;
+  };
+  return function() {
+    var now = Date.now();
+
+    var remaining = wait - (now - previous);
+    context = this;
+    args = arguments;
+    if (remaining <= 0 || remaining > wait) {
+      if (timeout) {
+        clearTimeout(timeout);
+        timeout = null;
+      }
+      previous = now;
+      result = func.apply(context, args);
+      if (!timeout) context = args = null;
+    } else if (!timeout) {
+      timeout = setTimeout(later, remaining);
+    }
+    return result;
+  };
+};
+
 // Animate the desktop navbar
 const parallax = () => {
   const pos = window.scrollY,
@@ -93,9 +124,5 @@ const init = () => {
   observer.observe(document.querySelector('#banner'));
 }
 
-document.addEventListener("DOMContentLoaded", evt => {
-  init();
-});
-document.addEventListener("scroll", evt => {
-  parallax();
-});
+document.addEventListener("DOMContentLoaded", init());
+document.addEventListener("scroll", throttle(parallax, 1000/50));
