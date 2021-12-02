23f2250e9d3a3fe48c01477dca5ee43211983fe7
Manu Chardonnens <manuchardonnens@Manus-MacBook-Pro.local>
2021-12-02 14:55:38 +0100

    revert complex throttle

--- new/assets/js/script.js
+++ new/assets/js/script.js
@@ -57,42 +57,18 @@ const containerObserverCallback = (entries, observer) => {
   });
 };
 
-const throttle = (func, wait, options) => {
-  let context,
-      args,
-      result;
-
-  let timeout = null,
-      previous = 0;
-
-  if (!options) options = {};
-
-  const later = () => {
-    previous = options.leading === false ? 0 : Date.now();
-    timeout = null;
-    result = func.apply(context, args);
-    if (!timeout) context = args = null;
-  };
-  return function() {
-    const now = Date.now();
-    if (!previous && options.leading === false) previous = now;
-    const remaining = wait - (now - previous);
-    context = this;
-    args = arguments;
-    if (remaining <= 0 || remaining > wait) {
-      if (timeout) {
-        clearTimeout(timeout);
-        timeout = null;
-      }
-      previous = now;
-      result = func.apply(context, args);
-      if (!timeout) context = args = null;
-    } else if (!timeout && options.trailing !== false) {
-      timeout = setTimeout(later, remaining);
+const throttle = (callback, limit) => {
+  let waiting = false;
+  return function () {
+    if (!waiting) {
+      callback.apply(this, arguments);
+      waiting = true;
+      setTimeout(function () {
+        waiting = false;
+      }, limit);
     }
-    return result;
-  };
-};
+  }
+}
 
 // Animate the desktop navbar
 const parallax = () => {
