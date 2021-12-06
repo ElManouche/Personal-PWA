33a35d7219739c48e122d9dabf9a723c3bf1a539
Manu Chardonnens <manuchardonnens@Manus-MacBook-Pro.local>
2021-12-06 13:29:35 +0100

    reduce unused css

--- new/assets/js/script.js
+++ new/assets/js/script.js
@@ -3,8 +3,8 @@ const linkClickedHandler = evt => {
   evt.preventDefault();
   const toggler = document.getElementById('toggler'),
         mobileOnlyElm = document.querySelector('.wrapper .mobile-navbar'),
-        isMobile = mobileOnlyElm.computedStyleMap().get('display').value === 'block',
-        timeout = ((isMobile && toggler.checked))? 300 : 10;
+        isMobile = mobileOnlyElm.style.display !== 'none',
+        timeout = ((isMobile && toggler.checked))? 400 : 10;
   let   target = evt.target;
 
   // Close the mobile menu
@@ -80,6 +80,10 @@ const parallax = () => {
           mg = document.querySelector("#banner .mg"),
           fg = document.querySelector("#banner .fg");
 
+    bg.style.backgroundImage = 'url("https://ik.imagekit.io/sarahdionne/hero/webp/20201024_143714_new_bg_ezVy20HPB.webp")';
+    mg.style.backgroundImage = 'url("https://ik.imagekit.io/sarahdionne/hero/webp/20201024_143714_new_mg_yX994Z_js.webp")';
+    fg.style.backgroundImage = 'url("https://ik.imagekit.io/sarahdionne/hero/webp/20201024_143714_new_fg_bYPFaTtl2.webp")';
+
     bg.style.top = `${pos*0.4}px`;
     bg.style.transform = `scale(${scale})`;
     mg.style.top = `${pos*0.3}px`;
@@ -96,9 +100,35 @@ const initLinksCloseNav = () => {
     link.addEventListener('click', linkClickedHandler);
   });
 }
-
+const toggleDisplay = (elm, prop = 'block', force = false) => {
+  if (elm.classList.contains('hidden') || force) {
+    elm.classList.add('visible');
+    elm.classList.remove('hidden');
+    //elm.style.display = prop;
+  } else {
+    elm.classList.remove('visible');
+    elm.classList.add('hidden');
+    //elm.style.display = "none";
+  }
+};
+const initCloseSubNav = () => {
+  document.querySelectorAll("li.submenu > span").forEach((item) => {
+    const defaultProp = 'block';
+    item.addEventListener('click', (evt) => {
+      const elm = evt.target.parentNode;
+      const ul = elm.querySelector('ul');
+      toggleDisplay(ul, defaultProp);
+    });
+    item.addEventListener('mouseover', (evt) => {
+      const elm = evt.target.parentNode;
+      const ul = elm.querySelector('ul');
+      toggleDisplay(ul, defaultProp, true);
+    });
+  });
+};
 const init = () => {
   initLinksCloseNav();
+  //initCloseSubNav();
   document.getElementById('toggler').addEventListener('click', toggleHandler);
   const observer = new IntersectionObserver(containerObserverCallback);
   observer.observe(document.querySelector('#banner'));
