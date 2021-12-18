75901f6c50d023d76a6c09abb096ed3b05cba360
Manu Chardonnens <manuchardonnens@Manus-MacBook-Pro.local>
2021-12-18 02:11:29 +0100

    Fake assets

--- sw.js
+++ sw.js
@@ -1,13 +1,18 @@
-const cacheName = "SD-v1.0.2",
+const cacheName = "SD-v1.0.5",
       assets = [
-        'assets/css/style.css',
-        'assets/js/script.js',
-        'index.html'
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/assets/js/script.js',
+        '/assets/css/style.css',
+        '/assets/icons/favicon.svg',
+        '/assets/icons/android-chrome-144x144.png'
       ];
 
 self.addEventListener('install', evt => {
   evt.waitUntil(
     caches.open(cacheName).then(cache => {
+      console.log('caches some stuff');
       cache.addAll(assets);
     })
   );
@@ -27,9 +32,11 @@ self.addEventListener('activate', evt => {
 self.addEventListener('fetch', evt => {
   if (evt.request.url.includes("?cc")) {
     caches.delete(cacheName);
+    console.log('delete all the cache');
   }
   evt.respondWith(
     caches.match(evt.request).then(cacheRes => {
+      console.log('Responding:', evt.request.url, cacheRes);
       return cacheRes || fetch(evt.request);
     })
   );
