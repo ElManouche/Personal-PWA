bd6348b7cf25cc1ec9e20a7e84a916807307fef5
Manu Chardonnens <manuchardonnens@Manus-MacBook-Pro.local>
2021-12-18 15:23:34 +0100

    try and skip

--- sw.js
+++ sw.js
@@ -1,18 +1,24 @@
-const cacheName = "SD-v1.0.5",
+const cacheName = "SD-v1.0.2",
       assets = [
         '/',
         '/index.html',
         '/manifest.json',
         '/assets/js/script.js',
         '/assets/css/style.css',
+        // Local images
         '/assets/icons/favicon.svg',
-        '/assets/icons/android-chrome-144x144.png'
+        '/assets/icons/android-chrome-144x144.png',
+        // CDN images
+        'https://ik.imagekit.io/sarahdionne/tr:w-30,fo-auto,f-webp/hero/full.jpg',
+        // Externals
+        'http://cdn.polyfill.io/v3/polyfill.min.js',
+        'https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&amp;display=swap'
       ];
 
 self.addEventListener('install', evt => {
   evt.waitUntil(
     caches.open(cacheName).then(cache => {
-      console.log('caches some stuff');
+      //console.log('caches some stuff');
       cache.addAll(assets);
     })
   );
@@ -29,15 +35,27 @@ self.addEventListener('activate', evt => {
   );
 });
 
+function handleErrors(response) {
+  if (!response.ok) {
+    //console.log('Houston, weve got a problem');
+    throw Error(response.statusText);
+  }
+  return response;
+}
+
 self.addEventListener('fetch', evt => {
   if (evt.request.url.includes("?cc")) {
     caches.delete(cacheName);
-    console.log('delete all the cache');
+    //console.log('delete all the cache');
+  }
+  if (evt.request.url.match(/\.(?:webp|png|jpg|jpeg|svg)$/)) {
+    //console.log('!!! it is an image !!!');
   }
   evt.respondWith(
     caches.match(evt.request).then(cacheRes => {
-      console.log('Responding:', evt.request.url, cacheRes);
-      return cacheRes || fetch(evt.request);
+      //console.log(`Responding ${(cacheRes? 'from cache' : 'go fetch!')}`, evt.request.url);
+      return cacheRes || fetch(evt.request)
+      //    .then(response => { console.log("ok"); return response; })
     })
   );
 });
