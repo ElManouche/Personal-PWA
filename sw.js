528c6d0ab32860c28b12945e87dfa95c8d32d84a
Manu Chardonnens <manuchardonnens@Manus-MacBook-Pro.local>
2021-12-18 01:21:17 +0100

    versioning of the SW

--- sw.js
+++ sw.js
@@ -1,15 +1,36 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('sw-cache').then(function(cache) {
-      return cache.add('index.html');
+const cacheName = "SD-v1.0.2",
+      assets = [
+        'assets/css/style.css',
+        'assets/js/script.min.js',
+        '/index.html'
+      ];
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
     })
   );
 });
- 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== cacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', evt => {
+  if (event.request.url.includes("?cc")) {
+    caches.delete(cacheName);
+  }
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
     })
   );
 });
