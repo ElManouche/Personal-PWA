b0306ba949d1108d25a0a31d3a0b208a71fc2d1b
ElManouche <me@elmanou.ch>
2023-01-24 17:28:05 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,210 @@
+// Underscore throttle function
+const throttle = (func, wait, options) => {
+  let timeout, context, args, result,
+      previous = 0;
+  if (!options) options = {};
+
+  const later = () => {
+    previous = options.leading === false ? 0 : new Date().getTime();
+    timeout = null;
+    result = func.apply(context, args);
+    if (!timeout) context = args = null;
+  };
+
+  const throttled = function() {
+    const now = new Date().getTime();
+    if (!previous && options.leading === false) previous = now;
+    const remaining = wait - (now - previous);
+    context = this;
+    args = arguments;
+    if (remaining <= 0 || remaining > wait) {
+      if (timeout) {
+        clearTimeout(timeout);
+        timeout = null;
+      }
+      previous = now;
+      result = func.apply(context, args);
+      if (!timeout) context = args = null;
+    } else if (!timeout && options.trailing !== false) {
+      timeout = setTimeout(later, remaining);
+    }
+    return result;
+  };
+
+  throttled.cancel = function() {
+    clearTimeout(timeout);
+    previous = 0;
+    timeout = context = args = null;
+  };
+  return throttled;
+};
+
+const trackEvent = (event, ...options) => {
+  console.log(`Track event: ${event}`, ...options);
+  if (typeof mixpanel === 'object') {
+    mixpanel.track(
+      event,
+      ...options
+    );
+  }
+};
+
 // JS TO USE THE MENU AS A SINGLE PAGE WITH SCROLL
-function subnavClickHandler(e) {
+const linkToAnchorClickedHandler = evt => {
+  evt.preventDefault();
   const toggler = document.getElementById('toggler');
-  if(toggler.checked === true) {
+  let   target = evt.target,
+        link;
+
+  // Close the mobile menu
+  if (!!toggler.checked) {
     setTimeout(() => toggler.click(), 10);
   }
 
-  const submenu = e.target.closest('.submenu');
+  if (target.nodeName !== 'A') {
+    target = target.closest("a");
+  }
+  
+  link = target.getAttribute('href');
+
+  // Scroll to the anchor
+  setTimeout(() => {
+    location.hash = "";
+    location.hash = link;
+  }, !!toggler.checked? 400 : 10);
+
+  const submenu = target.closest('li.submenu');
   if(!!submenu) {
     setTimeout(() => submenu.classList.add('closed'), 100);
-    setTimeout(() => submenu.classList.remove('closed'), 1000);   
+    setTimeout(() => submenu.classList.remove('closed'), 1000);
   }
-}
-function initLinksCloseNav() {  
-  document.querySelectorAll("a[href*='#'], li.submenu > span").forEach((anchor) => {
-    anchor.addEventListener('click', subnavClickHandler);
-  });
+  
+  trackEvent('Link to anchor clicked', {link: link});
+};
+
+// don't scroll body if the mobile menu is visible
+const togglerClickedHandler = checked => {
+  if(checked === true) {
+    document.documentElement.classList.add('no-scroll');
+  } else {
+    document.documentElement.classList.remove('no-scroll');
+  }
+};
+
+const initLinksCloseNav = () => {
+  document
+    .querySelectorAll("a[href*='#']:not([href='#'])")
+    .forEach(
+    (link) => link.addEventListener('click', linkToAnchorClickedHandler)
+  );
 }
 
-const initMaps = () => {
-  document.querySelectorAll('div.map').forEach(item => {
+const initCloseSubNav = () => {
+  document.querySelectorAll("li.submenu").forEach(item => {
+    const throttledReopenSubmenu = throttle(() => item.classList.remove('closed'), 1000, { leading: false });
     item.addEventListener('click', () => {
-      item.classList.add('interact');
-      const iframe = item.querySelector('iframe');
-      iframe.src = iframe.getAttribute('data-src');
-      iframe.removeAttribute('data-src');
+      item.classList.toggle('closed');
+      throttledReopenSubmenu.cancel();
     });
+    item.addEventListener('mouseenter', () => {
+      // on mobile, timeout to trigger hover after click ;)
+      setTimeout(() => item.classList.remove('closed'), 100);
+    });
+    item.addEventListener("mousemove", throttledReopenSubmenu);
   });
 };
 
-document.addEventListener("DOMContentLoaded", function(event) { 
+const initObserveElements = () => {
+  const elements = [].slice.call(document.querySelectorAll(".observable"));
+  const observablesObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        trackEvent(
+          'Object intersecting',
+          {
+            element: entry.target.nodeName,
+            class: entry.target.className
+          }
+        );
+        const dataset = entry.target.dataset;
+        for (const record in dataset) {
+          if (dataset[record]) {
+            entry.target.setAttribute(record, dataset[record]);
+            entry.target.removeAttribute(`data-${record}`);
+          }
+        }
+        observablesObserver.unobserve(entry.target);
+      }
+    });
+  });
+  elements.forEach(element => {
+    observablesObserver.observe(element);
+  });
+};
+
+const initNavPosition = () => {
+  const banner = document.getElementById('banner');
+  const bannerObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const container = document.querySelector(".container");
+      if(entry.isIntersecting) {
+        container.classList.add('banner-intersecting');
+      } else {
+        container.classList.remove('banner-intersecting');
+      }
+    });
+  });
+  if(!!banner) {
+    bannerObserver.observe(banner);
+  } else {
+    const container = document.querySelector(".container");
+    container.classList.remove('banner-intersecting');
+  }
+};
+
+const initMaps = () => {
+  document.querySelectorAll('div.map').forEach(item => {
+    const iframe = item.querySelector('iframe');
+    item.addEventListener('click', () => {
+      item.classList.add('interact');
+      iframe.src = iframe.getAttribute('data-src');
+      iframe.removeAttribute('data-src');
+    });
+    iframe.addEventListener('load', () => trackEvent("Map loaded", iframe.src), true);
+  });
+};
+
+const initTogglerListener = () => {
+  const toggler = document.getElementById('toggler');
+  toggler.addEventListener('click', () => togglerClickedHandler(toggler.checked) );
+};
+const trackFirstScroll = () => {
+  trackEvent('First scroll');
+  document.removeEventListener("scroll", trackFirstScroll);
+};
+
+const initFirstScrollListener = () => {
+  document.addEventListener("scroll", trackFirstScroll);
+};
+
+const initLinksClicked = () => {
+  document
+    .querySelectorAll("a[class*='contact_']")
+    .forEach(
+    (link) => link.addEventListener('click', () => trackEvent(`Link clicked`, {href: link.href}))
+  );
+};
+
+const init = () => {
+  initFirstScrollListener();
+  initObserveElements();
+  initTogglerListener();
   initLinksCloseNav();
+  initLinksClicked();
+  //initNavPosition();
+  initCloseSubNav();
   initMaps();
-});
+}
+
+document.addEventListener("DOMContentLoaded", init);
